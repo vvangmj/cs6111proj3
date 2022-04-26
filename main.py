dcb106741fab80ebb31f8d5ac307f50563170559
markwu372 <zwu372@gatech.edu>
2022-04-26 17:07:28 -0400

    output.txt format adjustment

--- main.py
+++ main.py
@@ -31,30 +31,41 @@ if __name__ == '__main__':
 
     data = read_file("INTEGRATED-DATASET.csv")
     large_item, conf_rules = get_large_item(data, min_sup, min_conf)
-    with open("output.txt", "w", newline="") as f:
-        writer = csv.writer(f, delimiter=" ")
-        writer.writerow(["=====Frequent itemsets (min_sup={})=====".format(min_sup)])
+    # with open("output.txt", "w", newline="") as f:
+    #     writer = csv.writer(f, delimiter=" ")
+    #     writer.writerow(["===============Frequent itemsets (min_sup={})===============".format(min_sup)])
+    text_file = open("output.txt", "w")
+    m = text_file.write("=============== Frequent Itemsets (min_sup={})===============\n".format(min_sup))
 
     print("Number of Large items:", len(large_item))
-    print("=====Frequent itemsets (min_sup={})=====".format(min_sup))
+    print("=============== Frequent Itemsets (min_sup={})===============\n".format(min_sup))
 
+    large_item = list(sorted(large_item, key=lambda x: x[1], reverse=True))
     for items in large_item:
-        print(items)
-        with open("output.txt", "a", newline="") as f:
-            writer = csv.writer(f, delimiter=" ")
-            writer.writerow([items[0],"{}%".format(items[1]*100)])
+        print(list(items[0]), ",", "{0:.4f}%".format(items[1]*100))
+        n = text_file.write("{0}, {1:.4f}%\n".format(list(items[0]), items[1]*100))
+        # with open("output.txt", "a", newline="") as f:
+        #     writer = csv.writer(f, delimiter=" ", quoting=csv.QUOTE_NONE, escapechar="\\")
+        #     writer.writerow(["{0}, {1:.4f}%".format(list(items[0]), items[1]*100)])
 
-    with open("output.txt", "a", newline="") as f:
-        writer = csv.writer(f, delimiter=" ")
-        writer.writerow(["=====High-confidence association rules (min_conf={})=====".format(min_conf)])
+    # with open("output.txt", "a", newline="") as f:
+    #     writer = csv.writer(f, delimiter=" ")
+    #     writer.writerow("")
+    #     writer.writerow(["===============High-confidence association rules (min_conf={})===============".format(
+    #         min_conf)])
+    n = text_file.write("\n")
+    m = text_file.write("=============== High-confidence Association Rules (min_conf={})===============\n".format(
+        min_conf))
 
     conf_rules = sorted(conf_rules, key=lambda x: x[3], reverse=True)
-    print("=====High-confidence association rules (min_conf={})=====".format(min_conf))
+    print("=============== High-confidence association rules (min_conf={})===============".format(min_conf))
     for rule in conf_rules:
         l, r, sup, conf = rule[0], rule[1], rule[2], rule[3]
-        print("{}=>{}(Conf:{},Supp:{})".format(l, r, conf, sup))
-        with open("output.txt", "a", newline="") as f:
-            writer = csv.writer(f, delimiter=" ")
-            writer.writerow(["{}=>{}(Conf:{}%,Supp:{}%)".format(l, r, conf*100, sup*100)])
+        print("{0}=>{1} (Conf:{2:.4f}%, Supp:{3:.4f}%)".format(l, r, conf*100, sup*100))
+        n = text_file.write("{0}=>{1} (Conf:{2:.4f}%, Supp:{3:.4f}%)\n".format(l, r, conf*100, sup*100))
+        # with open("output.txt", "a", newline="") as f:
+        #     writer = csv.writer(f, delimiter=" ", quoting=csv.QUOTE_NONE, escapechar="\\")
+        #     writer.writerow(["{0}=>{1} (Conf:{2:.4f}%, Supp:{3:.4f}%)".format(l, r, conf*100, sup*100)])
+    text_file.close()
 
 
